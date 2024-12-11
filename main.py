0bcd4364661c0b5409f9f24bc0bdefcfaaff7ce7
TheHawk <patricklehloenya2@gmail.com>
2024-12-11 09:31:46 +0200

    Fixed bug in num_words function to return len of list not the list and add a function to print a report

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    # num_words(file_contents)
-    count_char(file_contents)
+    number_of_words = num_words(file_contents)
+    count_char_occurence = count_char(file_contents)
+
+    print_report(number_of_words, count_char_occurence)
 
 
 def num_words(content):
     content_list = content.split()
 
-    return content_list
+    return len(content_list)
 
 
 def count_char(content):
@@ -24,6 +26,33 @@ def count_char(content):
             char_count[c] += 1
 
     return char_count
+
+
+def sort_on(char_count_dict):
+    for key in char_count_dict:
+        return char_count_dict[key]
+
+
+def print_report(num_words, char_count):
+
+    # create a list of only characters in the alphabet
+    list_of_alpha = []
+    for char in char_count:
+        if char.isalpha():
+            list_of_alpha.append({char: char_count[char]})
+
+    # sort them out
+    list_of_alpha.sort(reverse=True, key=sort_on)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for dic in list_of_alpha:
+        for char in dic:
+            print(f"The '{char}' character was found {dic[char]} times")
+
+    print("--- End report ---")
 
 
 if __name__ == '__main__':
